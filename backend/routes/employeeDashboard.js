0aa47b84e550902a8acd8353353392de793363d3
Ishankanungo <ishan.kanungo@sjsu.edu>
2021-11-27 18:15:00 -0800

    Created APIs in Employee Dashboard

--- backend/routes/employeeDashboard.js
+++ backend/routes/employeeDashboard.js
@@ -59,4 +59,70 @@ router.post("/addNewFlight", (req, res) => {
 
 })
 
+
+router.post("/updateFlightStatus", (req, res) => {
+    //console.log("Inside register", req.body)
+    const flightId = req.body.id
+    const newStatus=req.body.newStatus
+
+    // console.log(fname)
+
+    Flight.updateOne({_id: flightId}, {"status": newStatus}, { useFindAndModify: false }, (err, flight) => {
+        if (err) {
+            console.log(err)
+        }
+        if (flight) {
+            console.log("Flight updated successfully")
+            res.send(flight)
+
+        } else {
+            console.log("Flight not found")
+            res.send("Cannot update Status. Flight not found")
+        }
+    })
+
+})
+
+
+router.get("/fetchAllFlights",(req, res) => {
+    //console.log("Inside register", req.body)
+    Flight.find({},(err, flight) => {
+        if (err) {
+            console.log(err)
+        }
+        if (flight.length) {
+            console.log("Flight info fetched successfully")
+            res.send(flight)
+
+        } else {
+            console.log("Cannot find flight details")
+            res.send("Cannot find flight details")
+            }
+        })
+    })
+
+
+router.post("/updateFlightPrice", (req, res) => {
+    //console.log("Inside register", req.body)
+    const flightId = req.body.id
+    const newPrice=req.body.newPrice
+
+    // console.log(fname)
+
+    Flight.updateOne({_id: flightId}, {"price": newPrice}, { useFindAndModify: false }, (err, flight) => {
+        if (err) {
+            console.log(err)
+        }
+        if (flight) {
+            console.log("Flight price updated successfully")
+            res.send(flight)
+
+        } else {
+            console.log("Flight not found")
+            res.send("Cannot update Price. Flight not found")
+        }
+    })
+
+})
+
 module.exports = router;
