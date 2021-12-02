8e48a2a12c02ffb7b5085e661ab17ca63cb8473e
jash-05 <jash.sheth@gmail.com>
2021-12-02 13:51:49 -0800

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,27 @@
-const dotenv = require('dotenv')
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-dotenv.config()
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(cors())
-const bcrypt = require("bcrypt")
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+const bcrypt = require("bcrypt");
 
 // const people = []
 
-mongoose.connect(process.env.DB_CONNECT, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, () => {
-    console.log("DB connected")
-})
-console.log("Hello")
+mongoose.connect(
+	process.env.DB_CONNECT,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	() => {
+		console.log("DB connected");
+	}
+);
+console.log("Hello");
 // // Routes
 // app.get("/login", (req, res) => {
 //     res.json(people)
@@ -34,13 +38,13 @@ console.log("Hello")
 //     }
 // })
 
-app.use("/employeeDashboard",require("./routes/employeeDashboard"))
-app.use('/userLogin', require('./routes/userLogin'))
-app.use('/dashboard', require('./routes/dashboard'))
-app.use('/checkout', require('./routes/checkout'))
-app.use('/myBookings', require('./routes/myBookings'))
-app.use('/myProfile', require('./routes/myProfile'))
+app.use("/employeeDashboard", require("./routes/employeeDashboard"));
+app.use("/userLogin", require("./routes/userLogin"));
+app.use("/dashboard", require("./routes/dashboard"));
+app.use("/checkout", require("./routes/checkout"));
+app.use("/myBookings", require("./routes/myBookings"));
+app.use("/myProfile", require("./routes/myProfile"));
 
 app.listen(8000, () => {
-    console.log("BE started at port 8000")
-})
+	console.log("BE started at port 8000");
+});
